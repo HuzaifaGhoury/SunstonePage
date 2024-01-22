86ecf8d974f80e71883b31de6fd64f113907e65f
HuzaifaGhoury <90411909+HuzaifaGhoury@users.noreply.github.com>
2024-01-21 23:10:31 -0800

    feat: render application data in dashboard
    Add typed application properties and render names, client IDs, API
    credentials, and analytics entries in the applications view.

--- src/app/home/application.tsx
+++ src/app/home/application.tsx
@@ -1,11 +1,27 @@
-import { Button, Typography } from '@mui/material';
+import { Button, List, ListItem, ListItemText, Typography } from '@mui/material';
 import React from 'react';
 import styled from '../page.module.scss';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import Link from 'next/link';
 
-const Application = () => {
-  return (
+interface ApplicationProps {
+  applications?: {
+    name: string;
+    clientID: string;
+    apiCredentials: string;
+    analytics: string;
+  }[];
+}
+
+const Application: React.FC<ApplicationProps> = ({ applications = [] }) => {
+
+const hardcodedValues = [
+{ name: 'First name', clientID: 'fftt456765gjkkjhi83093985' },
+{ name: 'Second name', clientID: 'fftt456765gjkkjhi83093985' },
+{ name: 'Third name', clientID: 'fftt456765gjkkjhi83093985' },
+];
+
+return (
     <div>
       <div className={styled.applicationdiv}>
         <Typography className={styled.applicationheading}>Applications</Typography>
@@ -16,19 +32,51 @@ const Application = () => {
       <div className={styled.applicationcontent}>
         <div className={styled.applicationnames}>
           <Typography className={styled.namesclientgray}>Name</Typography>
-          <Typography className={styled.namesclient}>First app</Typography>
-          <Typography className={styled.namesclient}>Second app</Typography>
-          <Typography className={styled.namesclient}>Third app</Typography>
+          <List style={{ marginLeft:'-17px' , marginTop:'-15px' , fontFamily:'poppins'}}>
+            {hardcodedValues.map((value, index) => (
+              <ListItem key={index}>
+                <ListItemText primary={value.name} />
+              </ListItem>
+            ))}
+            {applications && applications.map((app, index) => (
+              <ListItem key={index + hardcodedValues.length}>
+                <ListItemText primary={app.name} />
+              </ListItem>
+            ))}
+          </List>
         </div>
         <div className={styled.applicationclient}>
           <Typography className={styled.namesclientgray}>Client ID</Typography>
-          <Typography className={styled.namesclient}>fftt456765gjkkjhi83093985</Typography>
-          <Typography className={styled.namesclient}>jk076590ygghgh324vd33</Typography>
-          <Typography className={styled.namesclient}>jk076590ygghgh324vd33</Typography>
+          <List style={{ marginLeft:'-17px' , marginTop:'-15px'}}>
+            {hardcodedValues.map((value, index) => (
+              <ListItem key={index} className={styled.namesclient}>
+                <ListItemText primary={value.clientID} />
+              </ListItem>
+            ))}
+            {applications && applications.map((app, index) => (
+              <ListItem key={index + hardcodedValues.length}>
+                <ListItemText primary={app.clientID} />
+              </ListItem>
+            ))}
+          </List>
         </div>
         <div className={styled.applicationbox}>
           <Typography className={styled.apicredential}>API Credentials</Typography>
+          <List>
+            {applications && applications.map((app, index) => (
+              <ListItem key={index}>
+                <ListItemText primary={app.apiCredentials} />
+              </ListItem>
+            ))}
+          </List>
           <Typography className={styled.apianalytic}>Analytics</Typography>
+          <List>
+            {applications && applications.map((app, index) => (
+              <ListItem key={index}>
+                <ListItemText primary={app.analytics} />
+              </ListItem>
+            ))}
+          </List>
         </div>
       </div>
       <div className={styled.dotbtn}>
